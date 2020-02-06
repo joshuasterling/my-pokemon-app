4255f28c6569f95e0e40cb716660be792015435c
Joshua Smith <joshuasmith@Joshs-MacBook-Pro.local>
2020-02-06 12:19:08 -0700

    completed backend rules

---
+++ server/controllers/pokemonController.js
@@ -0,0 +1,21 @@
+const caughtPokemon = [];
+
+let id = 0;
+
+module.exports = {
+  getCaughtPokemon: (req, res) => {
+
+  },
+
+  catchPokemon: (req, res) => {
+
+  },
+
+  editPokemonName: (req, res) => {
+
+  },
+
+  releasePokemon: (req, res) => {
+    
+  }
+}
---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const grassCtrl = require('./controllers/grassController');
+const pokemonCtrl = require('./controllers/pokemonController');
+
+const app = express();
+const PORT = 4050;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/wild-pokemon', grassCtrl.getWildPokemon);
+
+app.get('/api/pokemon', pokemonCtrl.getCaughtPokemon);
+app.post('/api/pokemon', pokemonCtrl.catchPokemon);
+app.put('/api/pokemon/:id', pokemonCtrl.editPokemonName);
+app.delete('/api/pokemon/:id', pokemonCtrl.releasePokemon);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
